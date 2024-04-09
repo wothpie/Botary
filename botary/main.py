6d596359ff8db2570c92f90cdbbb18bcbcf29ee1
Ahmet Can <119820009+wothpie@users.noreply.github.com>
2024-04-09 16:13:12 +0300

    Update main.py
    Hatalar düzeltildi.

--- botary/main.py
+++ botary/main.py
@@ -12,69 +12,64 @@ def get_external_ip():
 ip = get_external_ip()
 
 url = "https://api.projectbo.com.tr/"
-def GetBotaryTitle(API,  cek, S232, S243):
-    # HTTP isteği göndererek veriyi al
-    
-    f = "GetBotaryTitle"
-    # POST isteği göndererek fonksiyonu çağır
-    response = requests.post(url, data={
-        'func' : f,
- 
-        'API': API, # API
-        'ip' : ip,
-        'cek': cek, # çekilecek verinin where'ini oluşturur
-        'S232': S232, # kategori seçimi
-        'S243': S243 # kod ile veri çekme
-    })
 
-    # Yanıtı kontrol et
+def GetBotaryTitle(API, cek, S232, S243):
+    f = "GetBotaryTitle"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+    response = requests.post(url, data={
+        'func': f,
+        'API': API,
+        'cek': cek,
+         'ip' : ip,
+        'S232': S232,
+        'S243': S243
+    }, headers=headers)
+
     if response.status_code == 200:
-        return response.text  # Yanıtı bir değişkene ata
+        return response.text
     else:
-        print("Fonksiyon çağrısı başarısız oldu.")
+        print(f"Fonksiyon çağrısı başarısız oldu. Hata kodu: {response.status_code}")
+        print(f"SDCP yanıtı: {response.text}")
         return None
 
-
-def GetBotaryContent(API,  cek, S232, S243):
-    # HTTP isteği göndererek veriyi al
-    
+def GetBotaryContent(API, cek, S232, S243):
     f = "GetBotaryContent"
-    # POST isteği göndererek fonksiyonu çağır
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
     response = requests.post(url, data={
-        'func' : f,
+        'func': f,
+        'API': API,
+        'cek': cek,
+         'ip' : ip,
+        'S232': S232,
+        'S243': S243
+    }, headers=headers)
 
-        'API': API, # API
-        'ip' : ip,
-        'cek': cek, # çekilecek verinin where'ini oluşturur
-        'S232': S232, # kategori seçimi
-        'S243': S243 # kod ile veri çekme
-    })
-
-    # Yanıtı kontrol et
     if response.status_code == 200:
-        return response.text  # Yanıtı bir değişkene ata
+        return response.text
     else:
         print("Fonksiyon çağrısı başarısız oldu.")
         return None
 
 def GetBotaryOwner(API, cek, S232, S243):
-    # HTTP isteği göndererek veriyi al
-    
     f = "GetBotaryOwner"
-    # POST isteği göndererek fonksiyonu çağır
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
     response = requests.post(url, data={
-        'func' : f,
+        'func': f,
+        'API': API,
+        'cek': cek,
+         'ip' : ip,
+        'S232': S232,
+        'S243': S243
+    }, headers=headers)
 
-        'API': API, # API
-        'ip' : ip,
-        'cek': cek, # çekilecek verinin where'ini oluşturur
-        'S232': S232, # kategori seçimi
-        'S243': S243 # kod ile veri çekme
-    })
-
-    # Yanıtı kontrol et
     if response.status_code == 200:
-        return response.text  # Yanıtı bir değişkene ata
+        return response.text
     else:
         print("Fonksiyon çağrısı başarısız oldu.")
         return None
